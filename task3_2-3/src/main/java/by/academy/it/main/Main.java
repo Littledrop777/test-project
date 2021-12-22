a34bb7812b44981ea9cbbb6c415c10c73b8ec5d5
Anastasia <49166992+Littledrop777@users.noreply.github.com>
2021-12-22 10:42:42 +0300

    Delete task3_2-3 directory

--- task3_2-3/src/main/java/by/academy/it/main/Main.java
+++
@@ -1,9 +0,0 @@
-package by.academy.it.main;
-
-import by.academy.it.app.ApplicationMenu;
-
-public class Main {
-    public static void main(String[] args) {
-        ApplicationMenu.getInstance().printAvailableOptions();
-    }
-}
