267b659d198dafe3e5b2a73381f622bd3d18ae67
Анастасия Плеханова <Littledrop777@gmail.com>
2022-08-10 16:51:28 +0300

    changes

--- homework_2/task2_5-7/task5/src/main/java/by/academy/it/Main.java
+++
@@ -1,9 +0,0 @@
-package by.academy.it;
-
-import by.academy.it.context.Application;
-
-public class Main {
-    public static void main(String[] args) {
-        Application.start(args);
-    }
-}
--- homework_2/task2_5-7/task5/src/main/java/by/academy/it/exception/UnknownEntityException.java
+++
@@ -1,7 +0,0 @@
-package by.academy.it.exception;
-
-public class UnknownEntityException extends RuntimeException{
-
-    public UnknownEntityException(String message) {
-    }
-}
